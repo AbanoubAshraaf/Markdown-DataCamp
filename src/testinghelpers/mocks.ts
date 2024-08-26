2d7f7eced2cbac88f04f597316836971480d7162
AbanoubAshraaf <abanoubashrf25@gmail.com>
2024-08-26 22:56:16 +0200

    test: Add unit tests for useIsPortrait hook

---
+++ src/testinghelpers/mocks.ts
@@ -0,0 +1,15 @@
+export const mockedMarkDownText = 'markdown text'
+
+export const mockedPortraitDimensions = {
+    width: 400,
+    height: 800,
+    scale: 0,
+    fontScale: 0,
+};
+
+export const mockedLandScapeDimensions = {
+    width: 800,
+    height: 400,
+    scale: 0,
+    fontScale: 0,
+};
