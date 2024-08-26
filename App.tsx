78c64974ed6b55d74b1ec6f5a9ce47238333244b
AbanoubAshraaf <abanoubashrf25@gmail.com>
2024-08-26 23:03:46 +0200

    refactor: App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import SafeAreaWrapper from 'src/components/basicComponent/SafeAreaWrapper';
+import MarkdownEditor from 'src/components/markdown/MarkdownEditor';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaWrapper>
+      <MarkdownEditor />
+    </SafeAreaWrapper>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
